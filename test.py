877bad1fffd115fab486d4100330236759b168fd
bpeak <bpeakcpeak@gmail.com>
2020-03-07 20:34:15 +0900

    init

---
+++ test.py
@@ -0,0 +1,11 @@
+def sum(n):
+  total = 0
+  for i in range(1, n + 1):
+    total += i * i
+  # return n * ( n + 1 ) / 2
+  # total = 0
+  # for i in range(1, n + 1):
+  #   total += i
+  return total
+
+print(sum(10))
